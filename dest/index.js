11163fa8b128979a6d5bec5a905f19a77a57155f
山本勝也 <yamamoto@anysense.co.jp>
2018-06-03 11:59:21 +0900

    feat(flow): :tada: flowのお試しセット

---
+++ dest/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var fn = function fn(arg) {
+  console.log(arg);
+};
+
+fn('111');
---
+++ src/index.js
@@ -0,0 +1,10 @@
+/* @flow */
+
+/**
+ * @param  {string} arg
+ */
+const fn = (arg:string) => {
+  console.log(arg);
+}
+
+fn('111');
